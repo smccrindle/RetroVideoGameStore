269e0c5c3ec079c88d438fd8d50366c90c881848
Scott McCrindle <iainmccrindle@gmail.com>
2026-01-15 16:58:45 -0500

    Add project files.

---
+++ RetroVideoGameStore/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Models;
+
+
+namespace RetroVideoGameStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Use Category model to create fake list of 10 categories
+            // Create empty list of categories
+            var categories = new List<Category>();
+            // Loop to create 10 categories
+            for (int i =1; i <= 10; i ++)
+            {
+                categories.Add(new Category
+                {
+                    Id = i,
+                    Name = "Category " + i.ToString()
+                });
+            }
+            // Pass the list to the view for display
+            return View(categories);
+        }
+        public IActionResult Browse(string categoryName)
+        {
+            // Grab the category name passed in with the URL
+            ViewBag.categoryName = categoryName;
+            return View();
+        }
+    }
+}
---
+++ RetroVideoGameStore/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace RetroVideoGameStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
