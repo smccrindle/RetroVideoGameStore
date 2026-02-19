3477f3a40c1d1f8b4f334896d66c8ddca60dd66b
Scott McCrindle <iainmccrindle@gmail.com>
2026-02-19 17:20:33 -0500

    Completed Lesson 6 with shoppers, customers and admin authentication/authorization

--- RetroVideoGameStore/Controllers/CategoriesController.cs
+++ RetroVideoGameStore/Controllers/CategoriesController.cs
@@ -11,7 +11,7 @@ using RetroVideoGameStore.Models;
 
 namespace RetroVideoGameStore.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Administrator")]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
---
+++ RetroVideoGameStore/Controllers/ShopController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Data;
+
+namespace RetroVideoGameStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // dB connection
+        private readonly ApplicationDbContext _context;
+        
+        // Connect to dB whenever this controller is used
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Index()
+        {
+            // Get list of categories
+            var categories = _context.Categories.OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}
