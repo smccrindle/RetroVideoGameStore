27b2f54b187ff74a20c9bf92a6040e581b3a43e1
Scott McCrindle <iainmccrindle@gmail.com>
2026-04-09 16:56:27 -0400

    Completed unit tests for Index() of ProductsController

---
+++ RetroVideoGameStore/Controllers/ExampleController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RetroVideoGameStore.Controllers
+{
+    public class ExampleController : Controller
+    {
+        // Constructor method so instance of this class can be created
+        public ExampleController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ RetroVideoGameStoreTest/ExampleControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Controllers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace RetroVideoGameStoreTest
+{
+    [TestClass]
+    public class ExampleControllerTest
+    {
+        [TestMethod]
+        public void IndexReturnsSomething()
+        {
+            // Arrange
+            var controller = new ExampleController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexLoadsIndexView()
+        {
+            // Arrange - create new instance of ExampleController
+            var controller = new ExampleController();
+
+            // Act - we must cast the return type from IActionResult (generic) to a ViewResult (specific)
+            var result = (ViewResult)controller.Index();
+
+            // Assert - check that the ViewResult that is returned is actually called "Index"
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
