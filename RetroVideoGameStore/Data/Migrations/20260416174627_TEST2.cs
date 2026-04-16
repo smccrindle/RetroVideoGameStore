961f7badec5747120699c19993a98ab2807c3c05
Scott McCrindle <iainmccrindle@gmail.com>
2026-04-16 13:54:17 -0400

    Ready for exam

---
+++ RetroVideoGameStore/Data/Migrations/20260416174627_TEST2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RetroVideoGameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TEST2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
